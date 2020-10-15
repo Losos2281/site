04d71dcdef5a2d579020e05a1ee4740599a456d7
Comp0702 <Comp0702@tehnikum.ket.local>
2020-10-15 13:54:52 +0300

    dima

---
+++ index.php
@@ -0,0 +1,16 @@
+<title>Главная</title>
+<style>
+ a {
+ text-decoration:none;
+ }
+</style>
+<body>
+	<h1>
+		<a href="/">Разгуляев Дмитрий 4-2 ИС</a>
+	</h1>
+<ul>
+	<li><a href="/search.php">Поиск</a></li>
+	<li><a href="/login.php">Войти</a></li>
+	<li><a href="/register.php">Зарегистрироваться</a></li>
+</ul>
+</body>
---
+++ search.php
@@ -0,0 +1,18 @@
+<title>Поиск</title>
+<style>
+ a {
+ text-decoration:none;
+ }
+</style>
+<body>
+	<h1>
+		<a href="/">Разгуляев Дмитрий 4-2 ИС</a>
+	</h1>
+<form action="#" method="get" style="display: flex;">
+        <input type="text" name="s" placeholder="Запрос" required>
+        <input type="submit" value="Поиск">
+    </form>
+        <span>1</span>
+    <a href="?s=&page=2">Вперед ></a>
+<ul>
+</ul>
